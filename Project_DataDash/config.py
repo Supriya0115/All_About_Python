0a7cb83039322b3b8885b55768e5106dc44fe24e
Sup Sat <33270065+Supriya0115@users.noreply.github.com>
2018-07-01 23:28:04 -0400

    project files-bkp

---
+++ Project_DataDash/config.py
@@ -0,0 +1,4 @@
+consumer_key = 'b2jDtC9vAdTpnuJDZkNPkwEYF'
+consumer_secret = 'xEo1L3eRSaE6J7uOIlYP0ohoNp0jikv3LKunjp9arE0YWn5tVo'
+access_token = '57237295-K2EPPUBgXZfz83NuA92RxkzETWThD4YW8lt3bhAcW'
+access_token_secret = '9GD3xt89mMZNCKWmDwQvnaWw3tQjaz0vgz4l4d90Fb7N0'
